6df0701343ec7945fba332c73ea87fa4c6a18624
Jude Hung <jude@iotechsys.com>
2023-11-02 12:23:53 +0800

    EDX-4373 Add base error into go-mod-edge-utils
    Add error abstraction and base error implementation to represent internal errors
    for go-mod-edge-utils. This errors package is similar to
    https://github.com/edgexfoundry/go-mod-core-contracts/tree/main/errors with
    following differences:
    
    1. BaseError contains ErrDetails that can be used to store some error details, e.g.
       failed certification content, SQL state codes, to expose more error details for
       error receiver.
    2. Seperarte the BaseError implmentation and HTTPError, so that http status code can
       be properly managed on the HTTPError side.  In the future, if there
       is a requirement for specific type of error, e.g. MQTTError with
       ReasonCode, the BaseError can acts as the base implementation and the
       new error type can focus on its own error information.
    
    Signed-off-by: Jude Hung <jude@iotechsys.com>

---
+++ pkg/errors/base.go
@@ -0,0 +1,146 @@
+// Copyright (C) 2023 IOTech Ltd
+
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"runtime"
+)
+
+// BaseError generalizes an error structure which can be used for any type of error
+type BaseError struct {
+	// wrappedErr is a nested error which is used to form a chain of errors for better context
+	wrappedErr error
+	// kind contains information regarding the error type
+	kind ErrKind
+	// message contains detailed information about the error.
+	message string
+	// callerInfo contains information of function call stacks when this BaseError is invoked.
+	callerInfo string
+	// extensions contains extra information regarding the error
+	details ErrDetails
+}
+
+// Error implements the Error interface for the BaseError struct
+func (be BaseError) Error() string {
+	if be.wrappedErr == nil {
+		return be.message
+	}
+
+	// be.wrappedErr.Error functionality gets the error message of the wrapped error and which will handle both CommonEdgeX
+	// types and Go standard errors(both wrapped and non-wrapped).
+	if be.message != "" {
+		return be.message + " -> " + be.wrappedErr.Error()
+	} else {
+		return be.wrappedErr.Error()
+	}
+}
+
+// Message returns the first level error message without further details.
+func (be BaseError) Message() string {
+	if be.message == "" && be.wrappedErr != nil {
+		if w, ok := be.wrappedErr.(BaseError); ok {
+			return w.Message()
+		} else {
+			return be.wrappedErr.Error()
+		}
+	}
+
+	return be.message
+}
+
+// DebugMessages returns a string taking all nested and wrapped operations and errors into account.
+func (be BaseError) DebugMessages() string {
+	if be.wrappedErr == nil {
+		return be.callerInfo + ": " + be.message
+	}
+
+	if w, ok := be.wrappedErr.(BaseError); ok {
+		return be.callerInfo + ": " + be.message + " -> " + w.DebugMessages()
+	} else {
+		return be.callerInfo + ": " + be.message + " -> " + be.wrappedErr.Error()
+	}
+}
+
+// Kind returns the error kind of this BaseError.
+func (be BaseError) Kind() ErrKind {
+	return be.kind
+}
+
+// Details returns the detail maps of this BaseError.
+func (be BaseError) Details() ErrDetails {
+	return be.details
+}
+
+// AddDetail adds a detail associated with key for this BaseError.
+func (be BaseError) AddDetail(key string, detail any) {
+	if be.details == nil {
+		be.details = make(ErrDetails)
+	}
+	be.details[key] = detail
+}
+
+// Unwrap retrieves the next nested error in the wrapped error chain.
+// This is used by the new wrapping and unwrapping features available in Go 1.13 and aids in traversing the error chain
+// of wrapped errors.
+func (be BaseError) Unwrap() error {
+	return be.wrappedErr
+}
+
+// Is determines if an error is of type BaseError.
+// This is used by the new wrapping and unwrapping features available in Go 1.13 and aids the errors.Is function when
+// determining is an error or any error in the wrapped chain contains an error of a particular type.
+func (be BaseError) Is(err error) bool {
+	var baseError BaseError
+	switch {
+	case errors.As(err, &baseError):
+		return true
+	default:
+		return false
+	}
+}
+
+// Kind determines the Kind associated with an error by inspecting the chain of errors. The first non-KindUnknown Kind
+// found from the chain of wrappedErr is returned.  If Kind cannot be determined from wrappedErr, KindUnknown is returned
+func Kind(err error) ErrKind {
+	var e BaseError
+	if !errors.As(err, &e) {
+		return KindUnknown
+	}
+	// We want to return the first "Kind" we see that isn't Unknown, because
+	// the higher in the stack the Kind was specified the more context we had.
+	if e.kind != KindUnknown || e.wrappedErr == nil {
+		return e.kind
+	}
+	return Kind(e.wrappedErr)
+}
+
+// NewBaseError creates a new BaseError with the information provided
+func NewBaseError(kind ErrKind, errMsg string, err error, detail ErrDetails) BaseError {
+	return BaseError{
+		kind:       kind,
+		message:    errMsg,
+		wrappedErr: err,
+		callerInfo: getCallerInformation(),
+		details:    detail,
+	}
+}
+
+// ToBaseError creates a new BaseError with Kind found from err
+func ToBaseError(err error) BaseError {
+	kind := Kind(err)
+	return NewBaseError(kind, "", err, nil)
+}
+
+// getCallerInformation generates information about the caller function. This function skips the caller which has
+// invoked this function, but rather introspects the calling function 3 frames below this frame in the call stack. This
+// function is a helper function which eliminates the need for the 'callerInfo' field in the `CommonEdgeX` type and
+// providing an 'callerInfo' string when creating an 'CommonEdgeX'
+func getCallerInformation() string {
+	pc := make([]uintptr, 10)
+	runtime.Callers(3, pc)
+	f := runtime.FuncForPC(pc[0])
+	file, line := f.FileLine(pc[0])
+	return fmt.Sprintf("[%s]-%s(line %d)", file, f.Name(), line)
+}
---
+++ pkg/errors/http.go
@@ -0,0 +1,92 @@
+// Copyright (C) 2023 IOTech Ltd
+
+package errors
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HTTPError struct {
+	BaseError
+	httpStatusCode int
+}
+
+func NewHTTPError(wrappedError error) HTTPError {
+	kind := Kind(wrappedError)
+	baseError := NewBaseError(kind, "", wrappedError, nil)
+	return HTTPError{
+		BaseError:      baseError,
+		httpStatusCode: codeMapping(kind),
+	}
+}
+
+func (he HTTPError) Message() string {
+	return fmt.Sprintf("%s(status code:%d)", he.BaseError.Message(), he.httpStatusCode)
+}
+
+// codeMapping determines the correct HTTP response code for the given error kind.
+func codeMapping(kind ErrKind) int {
+	switch kind {
+	case KindUnknown, KindDatabaseError, KindServerError, KindOverflowError, KindNaNError:
+		return http.StatusInternalServerError
+	case KindCommunicationError:
+		return http.StatusBadGateway
+	case KindEntityDoesNotExist:
+		return http.StatusNotFound
+	case KindContractInvalid, KindInvalidId:
+		return http.StatusBadRequest
+	case KindStatusConflict, KindDuplicateName:
+		return http.StatusConflict
+	case KindLimitExceeded:
+		return http.StatusRequestEntityTooLarge
+	case KindServiceUnavailable:
+		return http.StatusServiceUnavailable
+	case KindServiceLocked:
+		return http.StatusLocked
+	case KindNotImplemented:
+		return http.StatusNotImplemented
+	case KindNotAllowed:
+		return http.StatusMethodNotAllowed
+	case KindRangeNotSatisfiable:
+		return http.StatusRequestedRangeNotSatisfiable
+	case KindIOError:
+		return http.StatusForbidden
+	case KindUnauthorized:
+		return http.StatusUnauthorized
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+// KindMapping determines the correct EdgeX error kind for the given HTTP response code.
+func KindMapping(code int) ErrKind {
+	switch code {
+	case http.StatusInternalServerError:
+		return KindServerError
+	case http.StatusBadGateway:
+		return KindCommunicationError
+	case http.StatusNotFound:
+		return KindEntityDoesNotExist
+	case http.StatusBadRequest:
+		return KindContractInvalid
+	case http.StatusConflict:
+		return KindStatusConflict
+	case http.StatusRequestEntityTooLarge:
+		return KindLimitExceeded
+	case http.StatusServiceUnavailable:
+		return KindServiceUnavailable
+	case http.StatusLocked:
+		return KindServiceLocked
+	case http.StatusNotImplemented:
+		return KindNotImplemented
+	case http.StatusMethodNotAllowed:
+		return KindNotAllowed
+	case http.StatusRequestedRangeNotSatisfiable:
+		return KindRangeNotSatisfiable
+	case http.StatusUnauthorized:
+		return KindUnauthorized
+	default:
+		return KindUnknown
+	}
+}
