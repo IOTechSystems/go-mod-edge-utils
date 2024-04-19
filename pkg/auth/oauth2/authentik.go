499637060b8becd6d2395d77fca0b7d39756f62b
Jack Chen <jack@iotechsys.com>
2024-04-19 15:17:55 +0800

    GUIT-994 Address review comment and add unit test
    Signed-off-by: Jack Chen <jack@iotechsys.com>

--- pkg/auth/oauth2/authentik.go
+++ pkg/auth/oauth2/authentik.go
@@ -14,16 +14,18 @@ import (
 	"golang.org/x/oauth2"
 
 	httpUtils "github.com/IOTechSystems/go-mod-edge-utils/pkg/http"
+	"github.com/IOTechSystems/go-mod-edge-utils/pkg/log"
 )
 
 type AuthentikAuthenticator struct {
 	Config      *oauth2.Config
 	UserInfoURL string
 	state       string
+	lc          log.Logger
 }
 
 type AuthentikUserInfo struct {
-	ID                string   `json:"sub"`
+	Sub               string   `json:"sub"`
 	Email             string   `json:"email"`
 	VerifiedEmail     bool     `json:"email_verified"`
 	Name              string   `json:"name"`
@@ -31,6 +33,9 @@ type AuthentikUserInfo struct {
 	PreferredUsername string   `json:"preferred_username"`
 	Nickname          string   `json:"nickname"`
 	Groups            []string `json:"groups"`
+
+	// Custom fields of a more common name for the user ID
+	UserID string `json:"id"`
 }
 
 // NewAuthentikConfigs returns an oauth2.Config object with the given parameters.
@@ -39,17 +44,18 @@ func NewAuthentikConfigs(clientId string, clientSecret string, redirectURL strin
 		ClientID:     clientId,
 		ClientSecret: clientSecret,
 		RedirectURL:  redirectURL,
-		Scopes:       []string{"openid", "profile", "email"}, // default and common scopes of Authentik
+		Scopes:       []string{"openid", "profile", "email"}, // default and common scopes of authentik
 		Endpoint:     endpoint,
 	}
 	return config
 }
 
-// NewAuthentikAuthenticator creates a new Authenticator for Authentik.
-func NewAuthentikAuthenticator(config *oauth2.Config, userInfoURL string) Authenticator {
+// NewAuthentikAuthenticator creates a new Authenticator for authentik.
+func NewAuthentikAuthenticator(config *oauth2.Config, userInfoURL string, lc log.Logger) Authenticator {
 	// state should be a random string to protect against CSRF attacks
 	state := uuid.NewString()
-	return &AuthentikAuthenticator{Config: config, UserInfoURL: userInfoURL, state: state}
+	lc.Debug("Initiating authentik authenticator.")
+	return &AuthentikAuthenticator{Config: config, UserInfoURL: userInfoURL, state: state, lc: lc}
 }
 
 // RequestAuth returns a http.HandlerFunc that redirects the user to the OAuth2 provider for authentication.
@@ -66,19 +72,24 @@ func (a *AuthentikAuthenticator) Callback() http.HandlerFunc {
 		code := r.URL.Query().Get(codeParam)
 		state := r.URL.Query().Get(stateParam)
 		if state != a.state {
-			http.Error(w, "invalid state. You may under CSRF attack.", http.StatusUnauthorized)
+			a.lc.Error("State does not match, you may be under CSRF attack.")
+			http.Error(w, "invalid state. You may be under CSRF attack.", http.StatusUnauthorized)
 			return
 		}
 
 		token, err := a.Config.Exchange(r.Context(), code)
+		a.lc.Debugf("exchange authentication code %v for the access token", code)
 		if err != nil {
+			a.lc.Errorf("failed to exchange token, err: %v", err)
 			http.Error(w, fmt.Sprintf("failed to exchange token, err: %v", err), http.StatusInternalServerError)
 			return
 		}
 
 		client := a.Config.Client(r.Context(), token)
 		resp, err := client.Get(a.UserInfoURL)
+		a.lc.Debugf("fetching user info from %v", a.UserInfoURL)
 		if err != nil {
+			a.lc.Errorf("failed to fetch user info: %v", err)
 			http.Error(w, fmt.Sprintf("failed to fetch user info: %v", err), http.StatusInternalServerError)
 			return
 		}
@@ -86,6 +97,7 @@ func (a *AuthentikAuthenticator) Callback() http.HandlerFunc {
 
 		userData, err := io.ReadAll(resp.Body)
 		if err != nil {
+			a.lc.Errorf("failed to read the response body: %v", err)
 			http.Error(w, fmt.Sprintf("fail to read the response body: %v", err), http.StatusInternalServerError)
 			return
 		}
@@ -93,18 +105,24 @@ func (a *AuthentikAuthenticator) Callback() http.HandlerFunc {
 		userInfo := AuthentikUserInfo{}
 		err = json.Unmarshal(userData, &userInfo)
 		if err != nil {
+			a.lc.Errorf("failed to parse the response body: %v", err)
 			http.Error(w, fmt.Sprintf("fail to parse the response body: %v", err), http.StatusInternalServerError)
 			return
 		}
 
+		// Use the 'sub' field from authentik as the user ID
+		userInfo.UserID = userInfo.Sub
+
 		err = userInfo.Validate()
 		if err != nil {
+			a.lc.Errorf("user info validation failed: %v", err)
 			http.Error(w, fmt.Sprintf("user info validation failed: %v", err), http.StatusUnauthorized)
 			return
 		}
 		httpUtils.WriteHttpHeader(w, r.Context(), http.StatusOK)
 		_, err = w.Write(userData)
 		if err != nil {
+			a.lc.Errorf("failed to write the response body: %v", err)
 			http.Error(w, fmt.Sprintf("fail to write the response body: %v", err), http.StatusInternalServerError)
 			return
 		}
--- pkg/auth/oauth2/authentik_test.go
+++ pkg/auth/oauth2/authentik_test.go
@@ -1,3 +1,167 @@
+//
+// Copyright (C) 2024 IOTech Ltd
+//
+
 package oauth2
 
-// placeholder
+import (
+	"golang.org/x/oauth2"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"testing"
+
+	"github.com/IOTechSystems/go-mod-edge-utils/pkg/log"
+)
+
+var (
+	mockSeverURL string
+)
+
+const (
+	mockServiceName  = "testService"
+	mockClientID     = "clientID"
+	mockClientSecret = "clientSecret"
+	mockAuthCode     = "authCode"
+	mockCallbackPath = "/callback"
+	mockAuthPath     = "/auth"
+	mockRedirectURL  = "http://localhost:8080" + mockCallbackPath
+)
+
+func getMockConfigs() *oauth2.Config {
+	return &oauth2.Config{
+		ClientID:     mockClientID,
+		ClientSecret: mockClientSecret,
+		RedirectURL:  mockRedirectURL,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  mockSeverURL + mockAuthPath,
+			TokenURL: mockSeverURL + mockTokenPath,
+		},
+	}
+}
+
+func newAuthentikAuthenticator() Authenticator {
+	logger := log.InitLogger(mockServiceName, log.InfoLog, nil)
+	configs := getMockConfigs()
+	return NewAuthentikAuthenticator(configs, mockSeverURL+mockUserInfoPath, logger)
+}
+
+func TestMain(m *testing.M) {
+	testMockServer := mockAuthenticServer()
+	defer testMockServer.Close()
+
+	URL, _ := url.Parse(testMockServer.URL)
+	mockSeverURL = "http://" + URL.Host
+
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
+
+func TestRequestAuth(t *testing.T) {
+	performRequestAuth(t)
+}
+
+func TestCallbackWithCorrectState(t *testing.T) {
+	authenticator, state := performRequestAuth(t)
+	rr := performCallback(t, state, authenticator)
+
+	// Check if the response status code is http.StatusOK (200)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	// Check if the response body contains the expected message
+	if body := rr.Body.String(); body != mockUserInfo {
+		t.Errorf("handler returned unexpected body: got %v want %v", body, mockUserInfo)
+	}
+}
+
+func TestCallbackWithIncorrectState(t *testing.T) {
+	authenticator := newAuthentikAuthenticator()
+	rr := performCallback(t, "", authenticator)
+
+	// Check if the response status code is http.StatusUnauthorized (401)
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusUnauthorized)
+	}
+
+	// Check if the response body contains the expected message
+	expectedMsg := "invalid state. You may be under CSRF attack.\n"
+	if body := rr.Body.String(); body != expectedMsg {
+		t.Errorf("handler returned unexpected body: got %v want %v", body, expectedMsg)
+	}
+}
+
+func performRequestAuth(t *testing.T) (Authenticator, string) {
+	authenticator := newAuthentikAuthenticator()
+	// Create a testing HTTP request
+	req, err := http.NewRequest("GET", mockAuthPath, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+
+	// Serve the RequestAuth HTTP request
+	authenticator.RequestAuth().ServeHTTP(rr, req)
+
+	// Check if the response status code is http.StatusFound (302)
+	if status := rr.Code; status != http.StatusFound {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusFound)
+	}
+
+	// Check if the Location header is set
+	location := rr.Header().Get("Location")
+	if location == "" {
+		t.Errorf("handler did not set Location header")
+	}
+
+	// Parse the location URL
+	parsedURL, err := url.Parse(location)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check if the domain, port, and path are correct
+	if "http://"+parsedURL.Host != mockSeverURL || parsedURL.Path != mockAuthPath {
+		t.Errorf("handler returned unexpected location URL: got %v want %s://%s:%s%s", location, parsedURL.Scheme, parsedURL.Hostname(), parsedURL.Port(), parsedURL.Path)
+	}
+
+	// Check if the state parameter exists
+	queryParams := parsedURL.Query()
+	stateParam := queryParams.Get("state")
+	if stateParam == "" {
+		t.Error("handler did not include state parameter in the URL")
+	}
+
+	return authenticator, stateParam
+}
+
+func performCallback(t *testing.T, state string, authenticator Authenticator) *httptest.ResponseRecorder {
+	if authenticator == nil {
+		authenticator = newAuthentikAuthenticator()
+	}
+
+	// Create a testing HTTP request
+	mockURL := mockCallbackPath
+	params := url.Values{}
+	params.Add("code", mockAuthCode)
+	if state != "" {
+		params.Add("state", state)
+		mockURL = mockCallbackPath + "?" + params.Encode()
+	}
+
+	req, err := http.NewRequest("GET", mockURL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+
+	// Serve the Callback HTTP request
+	authenticator.Callback().ServeHTTP(rr, req)
+	return rr
+}
